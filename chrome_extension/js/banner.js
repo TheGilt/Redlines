f2d32a36ef09da5539f1ba8a83364aa82eb4dae5
toby.dunkelberg <tdunk@cs.washington.edu>
2017-10-22 01:39:57 -0700

    Added logo section to banner

--- chrome_extension/js/banner.js
+++ chrome_extension/js/banner.js
@@ -1,7 +1,10 @@
 // USE AZURE TO FIND FAKE OR NOT
+// If no entry do nothing
 message = "";
 if (true) {
-    message = '<div class="redline-banner--fake"><div class="container"><div class="banner__title">Redlines Report</div><div class="banner__rating">FAKE</div></div></div>';
+    message = '<div class="redline-banner--fake"><div class="container">';
+    message += '<div class="banner__title"><img class="banner__title__logo" src="https://i.imgur.com/8Ma1Fnb.png" alt="logo">';
+    message += '<div class="banner__title__text">Redlines Report</div></div><div class="banner__rating">Our users marked this as fake</div></div></div>';
 } else {
     message = '<div class="redline-banner--fake"><div class="banner__title">Redlines</div><div class="banner--rating">FAKE</div></div>';
 }
