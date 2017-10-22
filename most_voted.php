4e0f27f60fc028345cbd5615b6e7c28b0efa8ecb
TheGilt <gilguday@outlook.com>
2017-10-22 13:53:56 -0700

    fixed

--- most_voted.php
+++ most_voted.php
@@ -1,39 +1,26 @@
 <?php
- 
-// Create connection
-$con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
+ $num_articles = $_POST['num_articles'];
 
-$num_articles = $_POST['num_articles'];
- 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+// Create connection
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+);
+$con = sqlsrv_connect($serverName, $connectionOptions);
+
  
 // This SQL statement selects ALL from the table 'Locations', but replace with whatever query is necessary
 $sql = "SELECT TOP " . $num_articles . " * FROM Articles as A ORDER BY A.downvotes + A.upvotes desc";
-if ($result = mysqli_query($con, $sql))
-{
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
- 
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
- 
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
+$getResults= sqlsrv_query($con, $sql);
+	
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+	echo json_encode($row);
 }
-
-
  
 // Close connections
-mysqli_close($con);
+sqlsrv_free_stmt($getResults);
 ?>
