d3eacaa20c62ff6cf45b5b926b104e917be72cf5
TheGilt <gilguday@outlook.com>
2017-10-22 12:23:37 -0700

    test

--- test.php
+++ test.php
@@ -1,15 +1,12 @@
 <?php
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+ 
+$con=sqlsrv_connect($serverName, $connectionOptions);
 
-$con=sqlsrv_connect("redlines.database.windows.net","redlines_dubhacks2017","redlines","Dubhacks17");
 
-$sql = "SELECT url_long FROM Articles"
-
-$getResults= sqlsrv_query($con, $sql);
-
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
- echo ($row['url_long'] . " " . PHP_EOL);
-}
-sqlsrv_free_stmt($getResults);
-// Close connections
 ?>
 
