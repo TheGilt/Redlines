058f39e2297fd853e0c69285ce301986b964d8de
TheGilt <gilguday@outlook.com>
2017-10-22 13:33:41 -0700

    search works

--- articles_by_search.php
+++ articles_by_search.php
@@ -2,36 +2,28 @@
 // get phrase input
 $phrase = $_GET['phrase']; 
 
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+);
+//Establishes the connection
+$con = sqlsrv_connect($serverName, $connectionOptions);
 // Create connection
-$con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
  
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+
  
 // This SQL statement selects ALL from the table 'Locations', but replace with whatever query is necessary
 $sql = "SELECT * FROM Articles as A WHERE A.title LIKE '%##" . $phrase . "##%'";
-if ($result = mysqli_query($con, $sql))
-{
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
- 
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
- 
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
-}
 
+$getResults= sqlsrv_query($con, $sql);
+
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+	echo json_encode($row);
+}
 
  
 // Close connections
