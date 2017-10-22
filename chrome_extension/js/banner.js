254c68c53dd0a22bd8ca63e4a017fa0a48b08b2c
toby.dunkelberg <tdunk@cs.washington.edu>
2017-10-22 10:46:38 -0700

    Changes look of tooltip

--- chrome_extension/js/banner.js
+++ chrome_extension/js/banner.js
@@ -1,7 +1,7 @@
 // USE AZURE TO FIND FAKE OR NOT
 // If no entry do nothing
 message = "";
-if (false) { // true == fake
+if (5 > 4) { // true == fake
     message = '<div class="redline-banner--fake redline-banner"><div class="container">';
     message += '<div class="banner__title"><img class="banner__title__logo" src="https://i.imgur.com/8Ma1Fnb.png" alt="logo">';
     message += '<div class="banner__title__text">Redlines Report</div></div><div class="banner__rating">Our users marked this as fake</div></div></div>';
--- chrome_extension/js/popup.js
+++ chrome_extension/js/popup.js
@@ -1,29 +1,38 @@
 // Inject the payload.js script into the current tab after the popout has loaded
-window.addEventListener('load', function (evt) {
-	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
-		file: 'payload.js'
-	});;
+window.addEventListener('load', function(evt) {
+    chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+        file: 'payload.js'
+    });;
 });
 
+var res;
+var upvotes;
+var downvotes;
+var title;
+var wiki;
+
 // Listen to messages from the payload.js script and write to popout.html
-chrome.runtime.onMessage.addListener(function (message) {
-	var url = message.url;
-	$.ajax({
-		url: 'http://localhost/article_info.php',
-	    type: "POST",
-	    dataType:'json', // add json datatype to get json
-	    data: ({urlLong: url}), 
-	    success: function(data) {
-	        var res = jQuery.parseJSON(data);
-	        var upvotes = res[0];
-	        var downvotes = res[1];
-	        var title = res[2];
-	        var wiki = res[3];
-	        // inject ratio of upvotes to downvotes to banner to decide which 
-	        // html to display
-	    },
-	    error: function(e) {
-	        alert("error");
-	    }
-	});
+chrome.runtime.onMessage.addListener(function(message) {
+    var url = message.url;
+    $.ajax({
+        url: 'http://localhost/article_info.php',
+        type: "POST",
+        dataType: 'json', // add json datatype to get json
+        data: ({ urlLong: url }),
+        success: function(data) {
+            var res = jQuery.parseJSON(data);
+            var upvotes = res[0];
+            var downvotes = res[1];
+            var title = res[2];
+            var wiki = res[3];
+            // inject ratio of upvotes to downvotes to banner to decide which 
+            // html to display
+        },
+        error: function(e) {
+            alert("error");
+        }
+    });
 });
+
+upvotes = 15;
+downvotes = 20;
