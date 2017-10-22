a3fc7a5699038946db90595f82e46308e3745a1e
TheGilt <gilguday@outlook.com>
2017-10-22 13:45:58 -0700

    fixed

--- has_user_voted.php
+++ has_user_voted.php
@@ -5,36 +5,27 @@ $url = $_GET['url'];
 
  
 // Create connection
-$con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
- 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+);
+//Establishes the connection
+$con = sqlsrv_connect($serverName, $connectionOptions);
  
 // This SQL statement selects ALL from the table 'Locations', but replace with whatever query is necessary
-$sql = "SELECT U.vote FROM UserVotes as U WHERE U.user_id = '##" . $user . "##' and U.url_long = '##" . $url . "##'";
-if ($result = mysqli_query($con, $sql))
-{
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
- 
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
- 
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
+$sql = "SELECT U.vote FROM UserVotes as U WHERE U.user_id = '" . $user . "' and U.url_long = '" . $url . "'";
+
+$getResults= sqlsrv_query($con, $sql);
+
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+	echo json_encode($row);
 }
 
  
 // Close connections
-mysqli_close($con);
+sqlsrv_free_stmt($getResults);
 ?>
