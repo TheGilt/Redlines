14b926c4815db361fb822b67f495c922decc60c9
TheGilt <gilguday@outlook.com>
2017-10-22 12:03:22 -0700

    test

--- test.php
+++ test.php
@@ -2,8 +2,12 @@
 
 $con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
  
-
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
  
 // Close connections
 mysqli_close($con);
 ?>
+
