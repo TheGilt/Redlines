0fa967c796d25bf3764d9772789401362c84a4c8
TheGilt <gilguday@outlook.com>
2017-10-22 12:21:17 -0700

    test

--- test.php
+++ test.php
@@ -1,12 +1,15 @@
 <?php
 
 $con=sqlsrv_connect("redlines.database.windows.net","redlines_dubhacks2017","redlines","Dubhacks17");
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+$sql = "SELECT url_long FROM Articles"
+
+$getResults= sqlsrv_query($con, $sql);
+
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['url_long'] . " " . PHP_EOL);
 }
- 
+sqlsrv_free_stmt($getResults);
 // Close connections
-mysqli_close($con);
 ?>
 
