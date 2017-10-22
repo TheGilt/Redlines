6f1cba0686b7b6f552b4fbbcba82857b456a2fc9
toby.dunkelberg <tdunk@cs.washington.edu>
2017-10-22 09:18:39 -0700

    banner image work

--- chrome_extension/js/banner.js
+++ chrome_extension/js/banner.js
@@ -1,11 +1,13 @@
 // USE AZURE TO FIND FAKE OR NOT
 // If no entry do nothing
 message = "";
-if (true) {
-    message = '<div class="redline-banner--fake"><div class="container">';
+if (false) { // true == fake
+    message = '<div class="redline-banner--fake redline-banner"><div class="container">';
     message += '<div class="banner__title"><img class="banner__title__logo" src="https://i.imgur.com/8Ma1Fnb.png" alt="logo">';
     message += '<div class="banner__title__text">Redlines Report</div></div><div class="banner__rating">Our users marked this as fake</div></div></div>';
 } else {
-    message = '<div class="redline-banner--fake"><div class="banner__title">Redlines</div><div class="banner--rating">FAKE</div></div>';
+    message = '<div class="redline-banner--true redline-banner"><div class="container">';
+    message += '<div class="banner__title"><img class="banner__title__logo" src="https://i.imgur.com/8Ma1Fnb.png" alt="logo">';
+    message += '<div class="banner__title__text">Redlines Report</div></div><div class="banner__rating">Our users marked this as fake</div></div></div>';
 }
 $(`html`).prepend(message);
