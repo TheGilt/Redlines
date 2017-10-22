eb97fa51b0cd11a524432db5ca2cb568503f8475
TheGilt <gilguday@outlook.com>
2017-10-22 12:43:04 -0700

    test

--- test.php
+++ test.php
@@ -9,5 +9,14 @@ $connectionOptions = array(
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 echo($conn);
+
+$tsql= "SELECT * FROM Articles";
+$getResults= sqlsrv_query($conn, $tsql);
+echo ("Reading data from table" . PHP_EOL);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['url_long'] . " " . $row['upvotes'] . PHP_EOL);
+}
 ?>
 
