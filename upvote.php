8cc7d53eade3512faee0f6594d89dcdabd0f6f76
TheGilt <gilguday@outlook.com>
2017-10-22 14:27:44 -0700

    fixed

--- upvote.php
+++ upvote.php
@@ -5,25 +5,26 @@ $urlShort = $_GET['urlShort'];
 $user_id = $_GET['user_id'];
 
 // Create connection
-$con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
- 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+);
+//Establishes the connection
+$con = sqlsrv_connect($serverName, $connectionOptions); 
+
  
 // This SQL statement selects ALL from the table 'Locations', but replace with whatever query is necessary
 
-$sql = "UPDATE Articles SET upvotes = upvotes + 1 WHERE url_long = '##" . $urlLong . "##';";
-mysqli_query($con, $sql);
+$sql = "UPDATE Articles SET upvotes = upvotes + 1 WHERE url_long = '" . $urlLong . "';";
+$getResults= sqlsrv_query($con, $sql);
 $sql = "INSERT INTO UserVotes (user_id, url_long, vote) VALUES 
-		('##" . $user_id . "##', '##" . $urlLong . "##', 1);";
-mysqli_query($con, $sql);
-$sql = "UPDATE WebSources SET upvotes = upvotes + 1 WHERE url_short = '##" . $urlShort . "##';";
-mysqli_query($con, $sql);
-
+		('" . $user_id . "', '" . $urlLong . "', 1);";
+$getResults= sqlsrv_query($con, $sql);
+$sql = "UPDATE WebSources SET upvotes = upvotes + 1 WHERE url_short = '" . $urlShort . "';";
+$getResults= sqlsrv_query($con, $sql);
  
 // Close connections
-mysqli_close($con);
+sqlsrv_free_stmt($getResults);
 ?>
