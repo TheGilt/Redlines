2e64985cfdd60dffc850c2ad4d0a5c948cc4941d
TheGilt <gilguday@outlook.com>
2017-10-22 12:41:17 -0700

    test

--- test.php
+++ test.php
@@ -1,6 +1,13 @@
 <?php
 echo("hello");
-
-
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+echo($conn);
 ?>
 
