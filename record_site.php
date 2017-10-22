cc3e41de52e369c6046e2cd166e95c0d11205d32
TheGilt <gilguday@outlook.com>
2017-10-22 14:12:51 -0700

    fixed

--- record_site.php
+++ record_site.php
@@ -5,32 +5,28 @@ $urlShort = $_GET['urlShort'];
 $title = $_GET['title'];
 
 // Create connection
-$con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
- 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+$serverName = "redlines.database.windows.net";
+$connectionOptions = array(
+    "Database" => "redlines_dubhacks2017",
+    "Uid" => "redlines",
+    "PWD" => "Dubhacks17"
+);
+//Establishes the connection
+$con = sqlsrv_connect($serverName, $connectionOptions);
+
  
 // This SQL statement selects ALL from the table 'Locations', but replace with whatever query is necessary
-$sql = "SELECT url_long FROM Articles as A WHERE A.url_long = '##" . $urlLong . "##';";
-$res = mysqli_query($con, $sql);
-if (count($res) = 0)
-{
-	$sql = "INSERT INTO dbo.Articles (url_long, upvotes, downvotes, title, wiki) VALUES 
-		('##" . $urlLong . "##', 0, 0, '##" . $title . "##', '');";
-	mysqli_query($con, $sql);
-	$sql = "SELECT url_short FROM WebSources as W WHERE W.url_short = '##" . $urlShort . "##';";
-	$res = mysqli_query($con, $sql);
-	if (count($res) = 0)
-	{
-		$sql = "INSERT INTO WebSources (url_short, upvotes, downvotes) 
-				VALUES ('##" . $urlShort . "##', 0);";
-		mysqli_query($con, $sql);
-	}
-}
+$sql = "SELECT url_long FROM Articles as A WHERE A.url_long = '" . $urlLong . "';";
+
+$getResults= sqlsrv_query($con, $sql);
+
+if (sizeof($getResults) == 0)
+	$sql = "INSERT INTO dbo.Articles (url_long, upvotes, downvotes, title, wiki)
+	VALUES ('" . urlLong . "', 0, 0, '" . title . "', '');";
+	$getResults= sqlsrv_query($con, $sql);
+
+
  
 // Close connections
-mysqli_close($con);
+sqlsrv_free_stmt($getResults);
 ?>
