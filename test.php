8436ad521103f75fe5f3a92100288adc066797c9
TheGilt <gilguday@outlook.com>
2017-10-22 12:13:07 -0700

    testconnect

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
 
-$con=mysqli_connect("redlines.database.windows.net","redlines","Dubhacks17","redlines_dubhacks2017");
- 
+$con=sqlsrv_connect("redlines.database.windows.net","redlines_dubhacks2017","redlines","Dubhacks17");
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
