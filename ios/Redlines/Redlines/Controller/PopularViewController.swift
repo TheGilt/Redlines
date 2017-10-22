ce5d50c9a657a4f00ea50a7efaf57a123f7884f0
Kennan Mell <kmell96@cs.washington.edu>
2017-10-21 23:23:14 -0700

    .

--- ios/Redlines/Redlines/Controller/PopularViewController.swift
+++ ios/Redlines/Redlines/Controller/PopularViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class PopularViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var mostViewedArticles: Array<Article>!
+    var mostVotedArticles: Array<Article>!
+    var sc = UISegmentedControl(items: ["Most Views", "Most Votes"])
+    
     var popularView: PopularView {
         return self.view as! PopularView
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mostVotedArticles = getMostVotedArticles()
+        mostViewedArticles = getMostViewedArticles()
+        sc.selectedSegmentIndex = 0
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,24 +37,36 @@ class PopularViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         if (indexPath.row != 0) {
-            // Not the segmented control
+            navigationController?.pushViewController(ArticleOneUpViewController(), animated: true)
         }
     }
 
     // MARK: UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if (sc.selectedSegmentIndex == 0) {
+            return mostViewedArticles.count + 1
+        } else {
+            // sc.selectedSegmentIndex == 1
+            return mostVotedArticles.count + 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let result = UITableViewCell()
+        let result: UITableViewCell
         if (indexPath.row == 0) {
-            let sc = UISegmentedControl(items: ["Most Views", "Most Votes"])
+            result = UITableViewCell()
             sc.center = CGPoint(x: self.view.center.x, y: result.center.y)
             sc.addTarget(self, action: #selector(PopularViewController.segmentedControlDidChangeValue), for: .valueChanged)
             sc.selectedSegmentIndex = 0
             result.addSubview(sc)
             result.selectionStyle = .none
+        } else {
+            result = ArticleTableViewCell()
+            if (sc.selectedSegmentIndex == 0) {
+                result.textLabel?.text = mostViewedArticles![indexPath.row - 1].title
+            } else {
+                result.textLabel?.text = mostVotedArticles![indexPath.row - 1].title
+            }
         }
         return result
     }
@@ -57,7 +77,17 @@ class PopularViewController: UIViewController, UITableViewDelegate, UITableViewD
         
     }
 
-    func getMostViewedArticles() {
-        
+    func getMostViewedArticles() -> Array<Article> {
+        var result = Array<Article>()
+        result.append(Article(title: "art1", url: "cnn.com/myarticle", source: "cnn", fakeVotes: 3, realVotes: 50, wiki: "I think this article is real cos it comes from CNN.", comments: Array<Comment>()))
+        result.append(Article(title: "art2", url: "nytimes.com/myarticle", source: "nytimes", fakeVotes: 0, realVotes: 5, wiki: "I think this article is real cos it comes from nyt.", comments: Array<Comment>()))
+        return result
+    }
+    
+    func getMostVotedArticles() -> Array<Article> {
+        var result = Array<Article>()
+        result.append(Article(title: "art3", url: "cnn.com/myarticle", source: "cnn", fakeVotes: 3, realVotes: 50, wiki: "I don't care about this article.", comments: Array<Comment>()))
+        result.append(Article(title: "art4", url: "nytimes.com/myarticle", source: "nytimes", fakeVotes: 0, realVotes: 5, wiki: "I think this article is fake yo.", comments: Array<Comment>()))
+        return result
     }
 }
--- ios/Redlines/Redlines/View/ArticleTableViewCell.swift
+++ ios/Redlines/Redlines/View/ArticleTableViewCell.swift
@@ -10,4 +10,24 @@ import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
     let fakeImageView = UIImageView(image: UIImage(named: "fake_stamp"))
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    private func initialize() {
+        accessoryType = .disclosureIndicator
+        addSubview(fakeImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        fakeImageView.frame = CGRect(x: self.frame.width * 0.9 - self.frame.height * 0.8, y: self.frame.height * 0.1, width: self.frame.height * 0.8, height: self.frame.height * 0.8)
+    }
 }
